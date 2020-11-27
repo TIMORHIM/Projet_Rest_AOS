0add055f827991b86a03104455872a4ab8daf5fb
Brahim TIMOR <brahim.timor@gmail.com>
2020-11-27 20:55:40 +0100

    Fix auth routes to swagger

--- src/main/java/aos/projet/HumanRessourceBo/security/WebSecurityConfig.java
+++ src/main/java/aos/projet/HumanRessourceBo/security/WebSecurityConfig.java
@@ -57,6 +57,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.cors().and().csrf().disable().
                 authorizeRequests()
+                .antMatchers("/**").permitAll()
                 .antMatchers("/api/**").authenticated()
                 .antMatchers("/api/auth/**").permitAll()
                 //.antMatchers("/(?!forms).*").permitAll()
